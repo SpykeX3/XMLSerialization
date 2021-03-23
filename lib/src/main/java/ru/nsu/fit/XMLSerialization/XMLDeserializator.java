de1d448492d70e118538cbdd9d0d77f78d377a13
katerina <bromkate@mail.ru>
2021-03-24 00:30:22 +0700

    Create XMLDeserializator.java

---
+++ lib/src/main/java/ru/nsu/fit/XMLSerialization/XMLDeserializator.java
@@ -0,0 +1,251 @@
+package ru.nsu.fit.XMLSerialization;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import sun.misc.Unsafe;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.*;
+import java.lang.reflect.Array;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class XMLDeserializator {
+
+  HashMap<Integer, Object> parsedObjects = new HashMap<>();
+  HashMap<Integer, Node> objectPool = new HashMap<>();
+
+  ArrayList<Integer> objectStream = new ArrayList<>();
+
+  public XMLDeserializator(InputStream inputStream)
+      throws InvalidClassException, ClassNotFoundException {
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    DocumentBuilder builder;
+    try {
+      builder = factory.newDocumentBuilder();
+    } catch (ParserConfigurationException e) {
+      e.printStackTrace();
+      return;
+    }
+    Document doc;
+    try {
+      doc = builder.parse(inputStream);
+    } catch (SAXException | IOException e) {
+      e.printStackTrace();
+      return;
+    }
+    doc.getDocumentElement().normalize();
+    Node pool = doc.getElementsByTagName("Object_pool").item(0);
+    Node stream = doc.getElementsByTagName("Object_stream").item(0);
+    createXMLObjectPool(pool);
+    createStreamIDList(stream);
+
+    System.out.println(Arrays.toString(objectPool.entrySet().toArray()));
+
+    for (Node node : objectPool.values()) {
+      parseObject(node);
+    }
+    System.out.println(Arrays.toString(parsedObjects.entrySet().toArray()));
+    for (Integer id : objectPool.keySet()) {
+      parseObjectFields(id);
+    }
+    System.out.println(Arrays.toString(parsedObjects.entrySet().toArray()));
+    /*
+    NodeList poolNodes = pool.getChildNodes();
+    for (int tag = 0; tag < poolNodes.getLength(); ++tag) {
+      Node bean = poolNodes.item(tag);
+      if (bean.getAttributes() == null) continue;
+      String type = bean.getAttributes().getNamedItem("type").getTextContent();
+      int id = Integer.parseInt(bean.getAttributes().getNamedItem("id").getTextContent());
+      if (type.contains("class ")) {
+        type = type.replaceAll("class ", "");
+      }
+
+      Class clazz = Class.forName(type);
+      Object createdObject = null;
+
+      Field f = null;
+      try {
+        f = Unsafe.class.getDeclaredField("theUnsafe");
+      } catch (NoSuchFieldException e) {
+        e.printStackTrace();
+      }
+      f.setAccessible(true);
+      Unsafe unsafe;
+      try {
+        unsafe = (Unsafe) f.get(null);
+      } catch (IllegalAccessException e) {
+        e.printStackTrace();
+        return;
+      }
+
+      createdObject = unsafe.allocateInstance(clazz);
+      // todo: отдельно парсить массивы и примитивы
+      // todo: использовать unsafe
+
+      }
+    }
+    */
+
+  }
+
+  private void createXMLObjectPool(Node pool) {
+    NodeList poolNodes = pool.getChildNodes();
+    for (int tag = 0; tag < poolNodes.getLength(); ++tag) {
+      Node bean = poolNodes.item(tag);
+      if (bean.getAttributes() == null) continue;
+      int id = Integer.parseInt(bean.getAttributes().getNamedItem("id").getTextContent());
+      objectPool.put(id, bean);
+    }
+  }
+
+  private void createStreamIDList(Node stream) {
+    NodeList streamNodes = stream.getChildNodes();
+    for (int tag = 0; tag < streamNodes.getLength(); ++tag) {
+      Node link = streamNodes.item(tag);
+      if (link.getAttributes() == null) continue;
+      int id = Integer.parseInt(link.getAttributes().getNamedItem("id").getTextContent());
+      objectStream.add(id);
+    }
+  }
+
+  private void parseObjectFields(int id) throws ClassNotFoundException {
+    Node bean = objectPool.get(id);
+    Object parsedObject = parsedObjects.get(id);
+    Class<?> clazz = parsedObject.getClass();
+
+    for (Field field : clazz.getDeclaredFields()) {
+      if (java.lang.reflect.Modifier.isStatic(field.getModifiers())) {
+        continue;
+      }
+      field.setAccessible(true);
+      String name = field.getName();
+      for (int i = 0; i < bean.getChildNodes().getLength(); ++i) {
+        if (bean.getChildNodes().item(i).getNodeName().equals(name)) {
+          Node fieldNode = bean.getChildNodes().item(i);
+          String fieldType = fieldNode.getAttributes().getNamedItem("type").toString();
+          if (fieldType.contains("class ")) {
+            if (fieldNode.getTextContent().equals("null")) {
+              try {
+                field.set(parsedObject, null);
+              } catch (IllegalAccessException ignored) {
+              }
+            } else {
+              int fieldId = Integer.parseInt(fieldNode.getTextContent());
+              try {
+                field.set(parsedObject, parsedObjects.get(fieldId));
+              } catch (IllegalAccessException ignored) {}
+              break;
+            }
+            break;
+          } else {
+            try {
+              switch (field.getType().toString()) {
+                case ("byte"):
+                  field.set(parsedObject, Byte.valueOf(fieldNode.getTextContent()));
+                  break;
+                case ("int"):
+                  field.set(parsedObject, Integer.valueOf(fieldNode.getTextContent()));
+                  break;
+                case ("float"):
+                  field.set(parsedObject, Float.valueOf(fieldNode.getTextContent()));
+                  break;
+                case ("double"):
+                  field.set(parsedObject, Double.valueOf(fieldNode.getTextContent()));
+                  break;
+                case ("short"):
+                  field.set(parsedObject, Short.valueOf(fieldNode.getTextContent()));
+                  break;
+                case ("long"):
+                  field.set(parsedObject, Long.valueOf(fieldNode.getTextContent()));
+                  break;
+                case ("boolean"):
+                  field.set(parsedObject, Boolean.valueOf(fieldNode.getTextContent()));
+                  break;
+                case ("char"):
+                  field.set(parsedObject, fieldNode.getTextContent().toCharArray()[0]);
+                  break;
+              }
+            } catch (IllegalAccessException ignored) {
+            }
+          }
+        }
+      }
+    }
+  }
+
+  private void parseObject(Node bean) throws ClassNotFoundException, InvalidClassException {
+    String type = bean.getAttributes().getNamedItem("type").getTextContent();
+    int id = Integer.parseInt(bean.getAttributes().getNamedItem("id").getTextContent());
+    if (type.contains("class ")) {
+      type = type.replaceAll("class ", "");
+    }
+
+    Class<?> clazz = Class.forName(type);
+    int length = 0;
+    // clazz.newInstance();
+    Object createdObject = null;
+
+    if (clazz.isArray()) {
+      length = Integer.parseInt(bean.getAttributes().getNamedItem("length").getTextContent());
+      Class<?> arrayType = clazz.getComponentType();
+      createdObject = Array.newInstance(arrayType, length);
+      parsedObjects.put(id, createdObject);
+      return;
+    }
+
+    for (Constructor<?> cons : clazz.getConstructors()) {
+      try {
+        cons.setAccessible(true);
+        if (cons.getParameterCount() == 0) {
+          createdObject = cons.newInstance();
+          parsedObjects.put(id, createdObject);
+          return;
+        }
+        if (clazz.isArray() && cons.getParameterCount() == 1) {
+          createdObject = cons.newInstance(length);
+          parsedObjects.put(id, createdObject);
+          return;
+        }
+      } catch (Exception ignored) {
+      }
+    }
+
+    Unsafe unsafe;
+    try {
+      Field f = null;
+      f = Unsafe.class.getDeclaredField("theUnsafe");
+      f.setAccessible(true);
+      unsafe = (Unsafe) f.get(null);
+    } catch (NoSuchFieldException | IllegalAccessException ignored) { // absolutely impossible
+      return;
+    }
+
+    try {
+      createdObject = unsafe.allocateInstance(clazz);
+      parsedObjects.put(id, createdObject);
+    } catch (InstantiationException e) {
+      throw new InvalidClassException(e.toString());
+    }
+    // todo: отдельно парсить массивы и примитивы
+
+  }
+
+  /* todo:
+  public boolean hasNext(){
+      return inputStream.
+  }*/
+  /*
+  public Object readObject(){
+
+  }
+  */
+
+}
