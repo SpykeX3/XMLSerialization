959cb2af7f883646d3c6a959790c59fd9d663925
SpykeX3 <spykex3@gmail.com>
2021-03-24 01:34:48 +0700

    add field and method scanner

---
+++ lib/src/main/java/ru/nsu/fit/XMLSerialization/ql/ValueLoader.java
@@ -0,0 +1,11 @@
+package ru.nsu.fit.XMLSerialization.ql;
+
+public class ValueLoader {
+    public ValueLoader(){
+
+    }
+
+    public void loadField(){
+
+    }
+}
---
+++ lib/src/test/java/ru/nsu/fit/XMLSerialization/Sandbox.java
@@ -0,0 +1,103 @@
+package ru.nsu.fit.XMLSerialization;
+
+import javassist.util.proxy.MethodHandler;
+import javassist.util.proxy.ProxyFactory;
+
+import org.junit.Test;
+import ru.nsu.fit.XMLSerialization.ql.QueryBuilder;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.function.Predicate;
+
+import static org.junit.Assert.fail;
+
+public class Sandbox {
+    public static class DummyClass {
+
+        static int A = 100500;
+
+        String greeting;
+        int a;
+        int b;
+
+        public DummyClass() {
+            greeting = "Hi!";
+            a = 1;
+            b = 2;
+        }
+
+        public DummyClass(String greet) {
+            greeting = greet;
+            a = 1;
+            b = 2;
+        }
+
+        void sayHello() {
+            System.out.println(greeting);
+        }
+
+        void increase() {
+            a++;
+            b++;
+        }
+    }
+
+    public static class AnotherDummyClass {
+        int c;
+        int d;
+
+        public AnotherDummyClass() {
+            c = 1;
+            d = 2;
+        }
+    }
+
+    public static DummyClass proxyTestClass() {
+        MethodHandler handler = (self, overridden, forwarder, args) -> {
+            if (overridden.getName().equals("sayHello")) {
+                System.out.println("Hello from proxy!");
+                forwarder.invoke(self, args);
+                ((DummyClass) self).greeting = "123";
+                forwarder.invoke(self, args);
+                return null;
+            }
+            return forwarder.invoke(self, args);
+        };
+
+        ProxyFactory factory = new ProxyFactory();
+        factory.setSuperclass(DummyClass.class);
+        try {
+            return (DummyClass) factory.create(new Class[]{String.class}, new Object[]{"Oh, hello there!"}, handler);
+        } catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+            fail();
+            return null;
+        }
+    }
+
+    @Test
+    public void testProxy() {
+        var test = proxyTestClass();
+        test.sayHello();
+    }
+
+
+    @Test
+    public void testQueryFieldFinder() {
+        AnotherDummyClass other = new AnotherDummyClass();
+        QueryBuilder qb = new QueryBuilder(null, DummyClass.class, new Predicate<Object>() {
+            @Override
+            public boolean test(Object o) {
+                DummyClass target = (DummyClass) o;
+                target.increase();
+                return outerFunction(target.a, target.b)
+                        && target.greeting.equals("test_value") && target.a < 40 && target.b < other.c;
+            }
+        });
+        qb.printDebugInfo();
+    }
+
+    private boolean outerFunction(int a, int b) {
+        return a > b;
+    }
+}
